016caa2b9b9646631a190b82807b14a8dec6c218
Massimo Donato <massimo.donato@polymtl.ca>
2023-09-05 20:25:26 -0400

    Modification a du server pour utiliser Scanner

--- Server/src/Client.java
+++ Server/src/Client.java
@@ -1,87 +1,25 @@
-import java.io.Console;
 import java.io.DataInputStream;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 // Application client
 public class Client {
 	private static Socket socket;
-	public static String serverAddress;
-	public static int serverPort;
-	public static String userName;
-	
-	public static void main(String[] args) throws Exception
-	{		
+	public static void main(String[] args) throws Exception {
 		// Adresse et port du serveur
-		setServerAddress();
-		setServerPort();
-		
-		// Nom d'utilisateur et mot de passe du client
-		setUser();
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
 		
 		// Création d'une nouvelle connexion aves le serveur
-		socket = new Socket(serverAddress, serverPort);
-		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, serverPort);
-		// Création d'un canal entrant pour recevoir les messages envoyés, par le serveur
+		socket = new Socket(serverAddress, port);
+		System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
+
+		// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
 		DataInputStream in = new DataInputStream(socket.getInputStream());
-		// Attente de la réception d'un message envoyé par le serveur sur le canal
+
+		// Attente de la réception d'un message envoyé par le, server sur le canal
 		String helloMessageFromServer = in.readUTF();
 		System.out.println(helloMessageFromServer);
+
 		// fermeture de La connexion avec le serveur
 		socket.close();
 	}
-
-	private static void setServerAddress() {
-		Console console = System.console();
-		boolean isCorrect = false;
-		Pattern pattern = Pattern.compile("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$");
-		
-		while (!isCorrect) {
-			serverAddress = console.readLine("Préciser l'adresse IP du poste sur lequel s'exécute le serveur : ");
-			Matcher matcher = pattern.matcher(serverAddress);
-			if (matcher.find()) {isCorrect = true;}
-			else {System.out.println("L'adresse du serveur est incorrecte : la plage d'attribution s'étend de 0.0.0.0 à 255.255.255.255.");}
-		}
-	}
-
-	private static void setServerPort() {
-		Console console = System.console();
-		boolean isCorrect = false;
-		
-		while (!isCorrect) {
-			serverPort = Integer.parseInt(console.readLine("Préciser le port d'écoute du serveur : "));
-			if (serverPort > 5000 || serverPort < 5050) {isCorrect = true;}
-			else {System.out.println("Le port du serveur est incorrect : il n'est pas compris entre 5000 et 5050.");}
-		}
-	}
-	
-	private static void setUser() {
-		Console console = System.console();
-		boolean nameIsCorrect = false;
-		boolean passwordIsCorrect = false;
-		
-		while (!nameIsCorrect) {
-			userName = console.readLine("Préciser l'adresse IP du poste sur lequel s'exécute le serveur : ");
-			if (isInDatabase(userName)) {nameIsCorrect = true;}
-			else {System.out.println("Le nom d'utilisateur est incorrect : il n'est pas enregistré dans la base de données.");}
-		}
-		
-		while (!passwordIsCorrect) {
-			char[] password = console.readPassword("To finish, enter password: ");
-			if (password.equals(getUserPassword(userName))) {passwordIsCorrect = true;}
-			else {System.out.println("Le mot de passe est incorrect : il ne correspond pas à celui associé au nom d'utilisateur.");}
-		}
-	}
-	
-	private static char[] getUserPassword(String userName) {
-		
-		return null;
-	}
-
-	private static boolean isInDatabase(String userName) {
-		
-		return false;
-	}
-
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,68 +1,80 @@
-import java.io.Console;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Scanner;
 
-// Application Serveur
+
 public class Server {
-	private static ServerSocket Listener;
-	public static String serverAddress;
-	public static int serverPort;
-
-	public static void main(String[] args) throws Exception
-	{		
+	private static ServerSocket Listener; // Application Serveur
+	
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
 		// Compteur incrémenté à chaque connexion d'un client au serveur
 		int clientNumber = 0;
-		
-		// Adresse et port du serveur
-		setServerAddress();
-		setServerPort();
 
-		// Création de la connexion pour communiquer avec les clients
+		// Adresse et port du serveur
+		String serverAddress = "127.0.0.1"; 
+		int serverPort = 5000; 
+		
+		serverAddress = getAddress(serverAddress, scanner);
+		serverPort = getPort(serverPort, scanner);
+		
+		
+		// Création de la connexien pour communiquer ave les, clients
 		Listener = new ServerSocket();
 		Listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		// Association de l'adresse et du port à la connexion
+		
+		// Association de l'adresse et du port à la connexien
 		Listener.bind(new InetSocketAddress(serverIP, serverPort));
 		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
 		
 		try {
-			// à chaque fois qu'un nouveau client se connecte, on exécute la fonction run() de l'objet ClientHandler
+			// À chaque fois qu'un nouveau client se, connecte, on exécute la fonstion
+			// run() de l'objet ClientHandler
+
 			while (true) {
-				// Important : la fonction accept() est bloquante et attend qu'un prochain client se connecte
-				// Une nouvelle connection : on incrémente le compteur clientNumber
-				new ClientHandler(Listener.accept(), clientNumber++).start();
+				// Important : la fonction accept() est bloquante: attend qu'un prochain client se connecte
+				// Une nouvetle connection : on incémente le compteur clientNumber new ClientHandler(Listener.accept(), clientNumber++).start();
 			}
-		}
-		finally {
-		// Fermeture de la connexion
-		Listener.close();
-		}
+		} finally {
+			// Fermeture de la connexion
+			Listener.close();
+		} 
 	}
-
-	private static void setServerAddress() {
-		Console console = System.console();
-		boolean isCorrect = false;
-		Pattern pattern = Pattern.compile("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$");
+	
+	public static String getAddress(String serverAddress, Scanner scanner) {
+		boolean isAcceptableAddress = false;
+		String patternString = "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$";
 		
-		while (!isCorrect) {
-			serverAddress = console.readLine("Préciser l'adresse IP du poste sur lequel s'exécute le serveur : ");
-			Matcher matcher = pattern.matcher(serverAddress);
-			if (matcher.find()) {isCorrect = true;}
-			else {System.out.println("L'adresse du serveur est incorrecte : la plage d'attribution s'étend de 0.0.0.0 à 255.255.255.255.");}
-		}
-	}
-
-	private static void setServerPort() {
-		Console console = System.console();
-		boolean isCorrect = false;
+		System.out.println("Enter your server address : ");
+		serverAddress = scanner.nextLine();
+		Pattern addressPattern = Pattern.compile(patternString);
+		Matcher hasPattern = addressPattern.matcher(serverAddress);
+		isAcceptableAddress = hasPattern.find();
 		
-		while (!isCorrect) {
-			serverPort = Integer.parseInt(console.readLine("Préciser le port d'écoute du serveur : "));
-			if (serverPort > 5000 || serverPort < 5050) {isCorrect = true;}
-			else {System.out.println("Le port du serveur est incorrect : il n'est pas compris entre 5000 et 5050.");}
+		if(!isAcceptableAddress) {
+			System.out.println("Invalid server address. Please try again!");
+			getAddress(serverAddress, scanner);
 		}
+		return serverAddress;
 	}
-}
+	
+	public static int getPort(int serverPort, Scanner scanner) {
+		boolean isAcceptablePort = false;
+		
+		System.out.println("Enter your server port : ");
+		serverPort = Integer.parseInt(scanner.nextLine());
+		if((serverPort >= 5000) && (serverPort <= 5050)) {
+			isAcceptablePort = true; 
+		}
+		else {
+			System.out.println("Invalid server port. Please try again!");
+			getPort(serverPort, scanner);
+		}
+		return serverPort;
+	}
+			
+};
